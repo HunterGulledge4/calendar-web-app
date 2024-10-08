27bbad9c36ef35a8acba94214be17f50e0f3593e
Hunter <huntergulledgebusiness@gmail.com>
2024-10-07 21:59:58 -0500

    initialized backend, and also fixed css issue that was causing it not to display

---
+++ app/config.py
@@ -0,0 +1,6 @@
+AWS_ACCESS_KEY_ID = 'AWSAccessKeyHere'
+AWS_SECRET_ACCESS_KEY = 'AWSSecretAccessKeyHere'
+REGION_NAME = 'us-east-2'
+
+CLIENT_ID = 'clientIdHere'
+USER_PASSWORD_AUTH = 'USER_PASSWORD_AUTH'
