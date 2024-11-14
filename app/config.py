56bc736af4ebbc63209b0a268aa9c57104016874
JaxPen <124705546+JaxPen@users.noreply.github.com>
2024-11-14 11:01:16 -0600

    Delete app directory to pull app.py to a different level

--- app/config.py
+++
@@ -1,6 +0,0 @@
-AWS_ACCESS_KEY_ID = 'AWSAccessKeyHere'
-AWS_SECRET_ACCESS_KEY = 'AWSSecretAccessKeyHere'
-REGION_NAME = 'us-east-2'
-
-CLIENT_ID = 'ClientIdHere'
-USER_PASSWORD_AUTH = 'USER_PASSWORD_AUTH'
